1c8d1a30fa35279f17652685a806cc9014237218
Emil Maran <emil@mycrm.de>
2016-09-14 14:20:08 +0200

    clone

--- get_git_list.php
+++ get_git_list.php
@@ -2,11 +2,11 @@
 /**
  * Created by PhpStorm.
  * User: emil
- * Date: 07.07.16
- * Time: 16:37
+ * Date: 14.09.16
+ * Time: 14:37
  */
 
-ini_set('error_reporting', 0);
+ini_set('error_reporting', 'E_ERROR');
 ini_set('display_errors', true);
 ini_set('display_startup_errors', true);
 
@@ -16,28 +16,33 @@ $TEAMNAME = $argv[2];
 $LIMITAPP = $argv[3];
 $MYPASSAP = $argv[4];
 $GITHUBAPP = "api.bitbucket.org";
-
-
-$URL = " -u {$USERNAME}:$MYPASSAP https://{$GITHUBAPP}/2.0/repositories/{$TEAMNAME}\?pagelen\={$LIMITAPP}";
-// curl -s  -u username:passwd https://api.bitbucket.org/2.0/repositories/teamname\?pagelen\=10
-
-# run curl in shell
-echo $cmd = "curl -s {$URL}";
-$resJson = shell_exec($cmd);
-$resArGit = (array) json_decode($resJson,true);
-//print "<pre>"; print_r($resArGit); die();
+$LIMITPGS = "2";
 
 # generate file
-//$fp = fopen("listprojects".date("YmdHis").".txt","w");
-$fp = fopen("listprojects.txt","w");
+#$txtFile = "listprojects_".date("YmdHis").".txt";
+$txtFile = "listprojects.txt";
+$fp = fopen($txtFile, "w");
 fwrite($fp, "");
 
-$fp = fopen("listprojects.txt","a");
-foreach($resArGit["values"] as $repoGit){
-	// https://username@bitbucket.org/username/emulaterepo.git.
-	$cloneUrl = $repoGit['links']['clone'][0]['href'];
-	$cloneUrl = str_replace("@bitbucket.org", ":".$MYPASSAP."@bitbucket.org", $cloneUrl);	// add pass in repo url
-	fwrite($fp, $cloneUrl."\n");
+for ($iPg = 1; $iPg <= $LIMITPGS; $iPg++) {
+
+	$URL = " -u {$USERNAME}:$MYPASSAP https://{$GITHUBAPP}/2.0/repositories/{$TEAMNAME}\?pagelen\={$LIMITAPP}\&page\=" . $iPg;
+// curl -s  -u username:passwd https://api.bitbucket.org/2.0/repositories/teamname\?pagelen\=10&&page=2
+
+# run curl in shell
+	echo $cmd = "curl -s {$URL}";
+	$resJson = shell_exec($cmd);
+	$resArGit = (array)json_decode($resJson, true);
+//print "<pre>"; print_r($resArGit); die();
+
+	$fp = fopen($txtFile, "a");
+	foreach ($resArGit["values"] as $repoGit) {
+		// https://username@bitbucket.org/username/emulaterepo.git.
+		$cloneUrl = $repoGit['links']['clone'][0]['href'];
+		$cloneUrl = str_replace("@bitbucket.org", ":" . $MYPASSAP . "@bitbucket.org", $cloneUrl);    // add pass in repo url
+		fwrite($fp, $cloneUrl . "\n");
+	}
+
 }
 
 /*
